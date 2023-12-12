9ec85ec3538454b46843124b670018d80f110f5e
HondaYt <honda.yuto.04@gmail.com>
2023-12-12 19:57:56 +0900

    環境構築とWelcomePageの開始。

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-export default function App(): JSX.Element {
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to TaskU!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.sectionContainer}>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.subTitle}>
+          大切なあなたの時間を守る、{"\n"}
+          タスク管理アプリ。
+        </Text>
+        <Text style={styles.title}>TaskU</Text>
+        {/* <button></button> */}
+      </SafeAreaView>
     </View>
   )
 }
+
 const styles = StyleSheet.create({
-  container: {
-    color: '#fff',
+  sectionContainer: {
+    // backgroundColor: "tomato",
     flex: 1,
-    // backgroundColor: '',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    padding: 10,
+  },
+  container: {
+    flexDirection: 'column', // 子要素を縦方向に並べます
+    gap: 10, // 子要素間のスペースを10ピクセルに設定します
+  },
+  subTitle: {
+    fontWeight: "500",
+    fontSize: 26,
+  },
+  title: {
+    fontSize: 66,
+    fontWeight: "600",
+  },
+});
+
+
+
+export default App
