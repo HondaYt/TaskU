ae299c874816bf86c5abc14dde0a81603c5dffbe
HondaYt <honda.yuto.04@gmail.com>
2023-12-18 19:51:19 +0900

    画面遷移テスト

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
@@ -8,6 +10,8 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -18,29 +22,65 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Welcome({ navigation }) {
   return (
     <View style={styles.sectionContainer}>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.subTitle}>
-          大切なあなたの時間を守る、{"\n"}
-          タスク管理アプリ。
-        </Text>
-        <Text style={styles.title}>TaskU</Text>
-        {/* <button></button> */}
+        <View>
+          <Text style={styles.subTitle}>
+            大切なあなたの時間を守る、{"\n"}
+            タスク管理アプリ。
+          </Text>
+          <Text style={styles.title}>TaskU</Text>
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={styles.getStartBtn}
+            onPress={() => navigation.navigate('Details')}>
+            <Text style={styles.getStartBtnText}>今すぐ始めよう</Text>
+          </TouchableOpacity>
+          <Text style={styles.tosText}>本サービスの利用開始をもって、{"\n"}
+            <Text style={styles.link}>利用規約</Text>と<Text style={styles.link}>プライバシーポリシー</Text>に同意したこととなります。</Text>
+        </View>
       </SafeAreaView>
     </View>
-  )
+  );
 }
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    // backgroundColor: "tomato",
     flex: 1,
+    backgroundColor: "#fff",
     padding: 10,
   },
   container: {
-    flexDirection: 'column', // 子要素を縦方向に並べます
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    // backgroundColor: "tomato",
+    flex: 1,
     gap: 10, // 子要素間のスペースを10ピクセルに設定します
   },
   subTitle: {
@@ -51,8 +91,30 @@ const styles = StyleSheet.create({
     fontSize: 66,
     fontWeight: "600",
   },
+  btnContainer: {
+    height: 200,
+    justifyContent: "space-between",
+    // backgroundColor: "blue",
+  },
+  getStartBtn: {
+    backgroundColor: "#333",
+    borderRadius: 8,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  getStartBtnText: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 18,
+  },
+  tosText: {
+    fontSize: 12,
+    textAlign: "center",
+    lineHeight: 18,
+  },
+  link: {
+    textDecorationLine: "underline",
+    color: "#555",
+  }
 });
-
-
-
-export default App
