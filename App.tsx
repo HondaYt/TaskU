b2f2f8ad9257dba901eb3a643185adce93a319b7
HondaYt <honda.yuto.04@gmail.com>
2023-12-19 17:52:06 +0900

    expoに環境移行

--- App.tsx
+++ App.tsx
@@ -24,34 +24,24 @@ import {
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
 
-function Welcome({ navigation }) {
+function Welcome({ navigation }: { navigation: any }) {
   return (
     <View style={styles.sectionContainer}>
       <SafeAreaView style={styles.container}>
-        <View>
-          <Text style={styles.subTitle}>
+        <View style={styles.ttlSection}>
+          <Text style={styles.subTtl}>
             大切なあなたの時間を守る、{"\n"}
             タスク管理アプリ。
           </Text>
-          <Text style={styles.title}>TaskU</Text>
+          <Text style={styles.ttl}>TaskU</Text>
         </View>
-        <View style={styles.btnContainer}>
+        <View style={styles.WelcomeBtnContainer}>
           <TouchableOpacity
             activeOpacity={0.8}
-            style={styles.getStartBtn}
+            style={styles.btn}
             onPress={() => navigation.navigate('Details')}>
-            <Text style={styles.getStartBtnText}>今すぐ始めよう</Text>
+            <Text style={styles.btnText}>今すぐ始めよう</Text>
           </TouchableOpacity>
           <Text style={styles.tosText}>本サービスの利用開始をもって、{"\n"}
             <Text style={styles.link}>利用規約</Text>と<Text style={styles.link}>プライバシーポリシー</Text>に同意したこととなります。</Text>
@@ -61,16 +51,35 @@ function Welcome({ navigation }) {
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }: { navigation: any }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+    <View style={styles.sectionContainer}>
+      <SafeAreaView style={styles.container}>
+        <View>
+
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={styles.btn}
+            onPress={() => navigation.navigate('Welcome')}>
+            <Text style={styles.btnText}>次に進む</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={[styles.btn, styles.prevBtn]}
+            onPress={() => navigation.navigate('Welcome')}>
+            <Text style={[styles.btnText, styles.prevBtnText]}>戻る</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
 
 
 const styles = StyleSheet.create({
+
   sectionContainer: {
     flex: 1,
     backgroundColor: "#fff",
@@ -83,32 +92,56 @@ const styles = StyleSheet.create({
     flex: 1,
     gap: 10, // 子要素間のスペースを10ピクセルに設定します
   },
-  subTitle: {
-    fontWeight: "500",
-    fontSize: 26,
+  ttlSection: {
+
   },
-  title: {
+  ttl: {
     fontSize: 66,
     fontWeight: "600",
   },
-  btnContainer: {
-    height: 200,
+  subTtl: {
+    fontWeight: "500",
+    fontSize: 26,
+    lineHeight: 35,
+  },
+  WelcomeBtnContainer: {
+    // height: 200,
+    gap: 8,
+    padding: 8,
     justifyContent: "space-between",
     // backgroundColor: "blue",
   },
-  getStartBtn: {
+  btnContainer: {
+
+    // height: 200,
+    gap: 8,
+    padding: 8,
+    // backgroundColor: "blue",
+  },
+  btn: {
     backgroundColor: "#333",
     borderRadius: 8,
     height: 60,
     justifyContent: "center",
     alignItems: "center",
   },
-  getStartBtnText: {
+  btnText: {
     color: "#fff",
     fontWeight: "600",
+    fontSize: 20,
+  },
+  prevBtn: {
+    backgroundColor: "#fff",
+    borderColor: "#555",
+    borderWidth: 3,
+  },
+  prevBtnText: {
+    color: "#555",
     fontSize: 18,
   },
   tosText: {
+    paddingTop: 8,
+    height: 60,
     fontSize: 12,
     textAlign: "center",
     lineHeight: 18,
@@ -118,3 +151,18 @@ const styles = StyleSheet.create({
     color: "#555",
   }
 });
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
